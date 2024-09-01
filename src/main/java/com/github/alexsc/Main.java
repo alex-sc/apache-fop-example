eae8159f158a391a69436c8ff525f09702a876f8
Alex <alex-sc@users.noreply.github.com>
2024-09-01 11:48:18 +0300

    Add example

---
+++ src/main/java/com/github/alexsc/Main.java
@@ -0,0 +1,51 @@
+package com.github.alexsc;
+
+import org.apache.fop.apps.FopFactory;
+import org.apache.fop.apps.Fop;
+import org.apache.fop.apps.MimeConstants;
+
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.sax.SAXResult;
+import javax.xml.transform.stream.StreamSource;
+import java.io.*;
+import java.nio.file.Files;
+
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+// Step 1: Construct a FopFactory by specifying a reference to the configuration file
+// (reuse if you plan to render multiple documents!)
+        FopFactory fopFactory = FopFactory.newInstance(new File("fop.xconf.xml").getAbsoluteFile());
+
+// Step 2: Set up output stream.
+// Note: Using BufferedOutputStream for performance reasons (helpful with FileOutputStreams).
+        OutputStream out = new BufferedOutputStream(new FileOutputStream(new File("output.pdf")));
+
+        try {
+            // Step 3: Construct fop with desired output format
+            Fop fop = fopFactory.newFop(MimeConstants.MIME_PDF, out);
+
+            // Step 4: Setup JAXP using identity transformer
+            TransformerFactory factory = TransformerFactory.newInstance();
+            Transformer transformer = factory.newTransformer(); // identity transformer
+
+            // Step 5: Setup input and output for XSLT transformation
+            // Setup input stream
+            Source src = new StreamSource(new File("ResultXML2FO.fo"));
+
+            // Resulting SAX events (the generated FO) must be piped through to FOP
+            Result res = new SAXResult(fop.getDefaultHandler());
+
+            // Step 6: Start XSLT transformation and FOP processing
+            transformer.transform(src, res);
+
+        } finally {
+            //Clean-up
+            out.close();
+        }
+    }
+}
